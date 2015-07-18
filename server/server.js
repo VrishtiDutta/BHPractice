6c6da723c3bdf87c8e005172ffb06b2a6f6d4fa7
Sugata <sugata.a@gmail.com>
2015-07-18 18:41:59 -0400

    frontend3

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var restify     =   require('restify');
 var mongojs     =   require('mongojs');
 var morgan  	=   require('morgan');
-var db          =   mongojs('bucketlistapp', ['appUsers','bucketLists']);
+var db          =   mongojs('mongodb://hello:hello@ds047692.mongolab.com:47692/roomtracker', ['appUsers','bucketLists']);
 var server      =   restify.createServer();
  
 server.use(restify.acceptParser(server.acceptable));
